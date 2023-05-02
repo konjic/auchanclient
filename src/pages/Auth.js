66981f5ea2e9ffce0927fee78f2047a87f902f3d
johnnythehobo <razvanburloiu@gmail.com>
2023-05-02 22:14:35 +0300

    fixes

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Register from '../components/Register'
 import Login from '../components/Login'
+import {useCookies} from 'react-cookie'
 
 function Auth() {
+  const [cookies,setCookies] = useCookies(["access_token"])
+
   return (
     <div>
-        <Register/>
-        <Login/>
+        {/* <Register/>
+        <Login/> */}
+
+        {!cookies.access_token? (<div><Register/>
+        <Login/></div>) 
+    : 
+    (<div> <h2> Hello {window.localStorage.getItem("name")  }. You are already logged in  </h2></div>)}
 
     </div>
   )
